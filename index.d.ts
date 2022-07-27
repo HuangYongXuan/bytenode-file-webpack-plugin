273d01d81f821d858c2caca122d02d8ef9c1d4c7
hyx <huangyx@singcheer.com>
2022-07-27 10:28:09 +0800

    update

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,7 @@ export interface Option {
     electron?: boolean;
     createLoader?: boolean;
     loaderFilename?: string;
+    callback?: Function
 }
 
 export default class ByteNodeFileWebpackPlugin {
--- index.js
+++ index.js
@@ -4,7 +4,6 @@
  * @date        2021/6/30 8:00 上午
  * @version     1.0
  */
-const path = require('path');
 const fs = require('fs');
 const bytenode = require('bytenode');
 
@@ -13,17 +12,23 @@ const bytenode = require('bytenode');
  * @constructor
  */
 function ByteNodeFileWebpackPlugin(options) {
-	this.options = options
+	this.options = options;
 }
 
 ByteNodeFileWebpackPlugin.prototype.apply = function (compiler) {
 	let self = this;
 	compiler.plugin('done', function () {
 		self.options.forEach(option => {
-			bytenode.compileFile(option)
-		})
+			fs.readFile(option.filename, 'utf8', function (err, data) {
+				bytenode.compileFile(option);
+				if (option.callback) {
+					data = option.callback(data);
+				}
+				fs.writeFileSync(option.filename, data);
+			});
+		});
 	});
 
-}
+};
 
 module.exports = ByteNodeFileWebpackPlugin;
